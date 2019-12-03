dfba51a1bdbf3304cb8d555038991a3f35121519
innaznayda <innaznayda@gmail.com>
2019-12-04 00:12:32 +0200

    day 3 solved

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -9,10 +9,15 @@ namespace Day3 {
         private static string[] secondWire;
 
         class Point {
-            public Point(int v1, int v2,int v3) {
+            public Point(int v1, int v2, int v3) {
                 this.X = v1;
                 this.Y = v2;
                 this.Steps = v3;
+            }
+            public Point(int v1, int v2) {
+                this.X = v1;
+                this.Y = v2;
+
             }
 
             public int X { get; set; }
@@ -20,37 +25,28 @@ namespace Day3 {
             public int Steps { get; set; }
         }
 
-        class PointComparer : IEqualityComparer<Point> {
-            // Products are equal if their names and product numbers are equal.
+        class PointComparer : IEqualityComparer<Point> {            
             public bool Equals(Point x, Point y) {
-
-
-                //Check whether the products' properties are equal.
-                return x.X == y.X && x.Y == y.Y && x.Steps <= y.Steps;
+                return x.X == y.X && x.Y == y.Y;
             }
-
-
-            // If Equals() returns true for a pair of objects 
-            // then GetHashCode() must return the same value for these objects.
-
-            public int GetHashCode(Point product) {
+            
+            public int GetHashCode(Point point) {
                 //Check whether the object is null
-                if (Object.ReferenceEquals(product, null)) return 0;
+                if (Object.ReferenceEquals(point, null)) return 0;
 
                 //Get hash code for the Name field if it is not null.
-                int hashProductName = product.X == null ? 0 : product.X.GetHashCode();
+                int hashProductName = point.X == null ? 0 : point.X.GetHashCode();
 
                 //Get hash code for the Code field.
-                int hashProductCode = product.Y.GetHashCode();
+                int hashProductCode = point.Y.GetHashCode();
 
                 //Calculate the hash code for the product.
                 return hashProductName ^ hashProductCode;
             }
-
         }
 
         static void Main(string[] args) {
-            GetContent();            
+            GetContent();
             var firstPath = BuildPath(fisrtWire);
             var secondPath = BuildPath(secondWire);
 
@@ -58,40 +54,46 @@ namespace Day3 {
 
             Console.WriteLine(common.Select(point => CalculateManhattanDistanceToCenter(point.X, point.Y)).Min());
 
-            //????????Console.WriteLine(common.Select(point => point.Steps).Min());
+            List<int> steps = new List<int>();
+            foreach (var elem in common) {
+                var f = firstPath.Where(el => el.X == elem.X && el.Y == elem.Y).First().Steps + secondPath.Where(el => el.X == elem.X && el.Y == elem.Y).First().Steps;
+                steps.Add(f);
+            }
+
+            Console.WriteLine(steps.Min());
             Console.ReadLine();
         }
 
         private static List<Point> BuildPath(string[] wire) {
             List<Point> points = new List<Point>();
-            var fisrtPoint = new Point(0, 0,0);
+            var fisrtPoint = new Point(0, 0, 0);
             points.Add(fisrtPoint);
             foreach (var s in wire) {
                 var number = Int32.Parse(s.Remove(0, 1));
                 switch (s[0]) {
-                    case 'R':                        
+                    case 'R':
                         var rng = Enumerable.Range(points.Last().X + 1, number);
                         foreach (var x in rng) {
-                            points.Add(new Point(x, points.Last().Y, points.Last().Steps + number));
+                            points.Add(new Point(x, points.Last().Y, points.Last().Steps + 1));
                         }
 
                         break;
                     case 'L':
                         var rng2 = Enumerable.Range(points.Last().X - number, number).Reverse();
                         foreach (var x in rng2) {
-                            points.Add(new Point(x, points.Last().Y, points.Last().Steps + number));
-                        }                        
+                            points.Add(new Point(x, points.Last().Y, points.Last().Steps + 1));
+                        }
                         break;
                     case 'U':
-                        var rng3 = Enumerable.Range(points.Last().Y+1, number);
+                        var rng3 = Enumerable.Range(points.Last().Y + 1, number);
                         foreach (var y in rng3) {
-                            points.Add(new Point(points.Last().X, y, points.Last().Steps + number));
+                            points.Add(new Point(points.Last().X, y, points.Last().Steps + 1));
                         }
                         break;
                     case 'D':
                         var rng4 = Enumerable.Range(points.Last().Y - number, number).Reverse();
                         foreach (var y in rng4) {
-                            points.Add(new Point(points.Last().X, y, points.Last().Steps + number));
+                            points.Add(new Point(points.Last().X, y, points.Last().Steps + 1));
                         }
                         break;
                     default: break;
@@ -101,14 +103,13 @@ namespace Day3 {
         }
 
         private static void GetContent() {
-            // text = File.ReadAllText("InputFile.txt");
-            string text = "R8,U5,L5,D3\r\nU7,R6,D4,L4";
+            string text = File.ReadAllText("InputFile.txt");
             var allText = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
             fisrtWire = allText[0].Split(new[] { ',' });
             secondWire = allText[1].Split(new[] { ',' }); ;
         }
 
-        public static int CalculateManhattanDistanceToCenter(int x, int y) {
+        private static int CalculateManhattanDistanceToCenter(int x, int y) {
             return Math.Abs(x - 0) + Math.Abs(y - 0);
         }
     }
