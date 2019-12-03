32f4be54a522d2a52aa0d1df3167a3b7b1e38fe2
innaznayda <innaznayda@gmail.com>
2019-12-03 19:41:37 +0200

    first day

---
+++ Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1 {
+    class Program {
+        static void Main(string[] args) {
+            //read ana parse input
+            var input = GetContent();
+            //calculate fuel for each module and sum up      
+
+            Console.WriteLine(input.Select(module =>CalculateFuelForModule(module)).Sum());
+            Console.WriteLine(input.Select(module => CalculateFuelForModuleAndFuel(module)).Sum());           
+
+            Console.ReadLine();
+
+        }
+
+        private static List<int> GetContent() {
+            string text = File.ReadAllText("InputFile.txt");            
+            var moduleMassText = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            return moduleMassText.Select(el => Int32.Parse(el)).ToList();
+
+        }
+
+        private static int CalculateFuelForModule(int mass) {   
+            
+            return mass / 3 - 2;
+        }
+
+        private static int CalculateFuelForModuleAndFuel(int mass) {
+            var result = 0;
+            int fuel = CalculateFuelForModule(mass);
+            while (fuel >= 0) {
+                result += fuel;
+                fuel = CalculateFuelForModule(fuel);
+            }
+
+            return result;
+        }
+    }
+}
