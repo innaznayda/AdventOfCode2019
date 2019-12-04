487ac3da63b47057dc987464b56029b329427df9
innaznayda <innaznayda@gmail.com>
2019-12-04 20:54:18 +0200

    Day 4, part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -5,21 +5,25 @@ namespace Day4 {
     class Program {
         static void Main(string[] args) {
             int counter = 0;
-            for(int i = 145852;i <= 616942;i++) {
+            int counterAdvanced = 0;
+            for (int i = 145852; i <= 616942; i++) {
                 var digits = i.ToString().ToCharArray().Select(el => Int32.Parse(el.ToString())).ToArray();
-                if(SameAdjacentDigits(digits) && NeverDecreaseDigits(digits)) {
+                if (SameAdjacentDigits(digits) && NeverDecreaseDigits(digits)) {
                     counter++;
+                }
+                if (SameAdjacentDigitsAdvanced(digits) && NeverDecreaseDigits(digits)) {
+                    counterAdvanced++;
                 }
 
             }
-
             Console.WriteLine(counter);
+            Console.WriteLine(counterAdvanced);
             Console.ReadLine();
         }
 
         private static bool NeverDecreaseDigits(int[] digits) {
-            for(int i = 0;i < digits.Length - 1;i++) {
-                if(digits[i] > digits[i + 1]) {
+            for (int i = 0; i < digits.Length - 1; i++) {
+                if (digits[i] > digits[i + 1]) {
                     return false;
                 }
             }
@@ -27,12 +31,28 @@ namespace Day4 {
         }
 
         private static bool SameAdjacentDigits(int[] digits) {
-            for(int i = 0;i < digits.Length - 1;i++) {
-                if(digits[i] == digits[i + 1]) {
+            for (int i = 0; i < digits.Length - 1; i++) {
+                if (digits[i] == digits[i + 1]) {
                     return true;
                 }
             }
             return false;
+        }
+
+        private static bool SameAdjacentDigitsAdvanced(int[] digits) {
+            int counter = 1;
+            bool found = false;
+            for (int i = 0; i < digits.Length - 1; i++) {
+                if (digits[i] == digits[i + 1]) {
+                    counter++;
+                } else {
+                    if (counter == 2) {
+                        found = true;
+                    }
+                    counter = 1;
+                }
+            }
+            return found || counter == 2;
         }
     }
 }
